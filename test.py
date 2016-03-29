73526b2e892f61bdfaabf9d1402d85a13bd991aa
Jiskejanssen <jiske@janssen.lu>
2016-03-29 16:39:56 +0200

    Revert "Some changes by Jiske"
    This reverts commit 2f7a01b6d194dae46efebd6c22e7d68eff4ff674.

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 print "Hello world"
 
-for i in range (0, 5):
+for i in range (0, 5)
   result = i + 1
   print result
 
-print "Jiske made some changes muhahaha"
+http://www.mtnmath.com/whatrh/node66.html
