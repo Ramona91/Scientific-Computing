68a699bc74c5cc51651a6ea389a6da1d3880c64e
Jiskejanssen <jiske@janssen.lu>
2016-04-08 14:16:50 +0200

    With working Jacobi, Gauss-Seidel and Successive over relaxation

--- set1-G.py
+++ set1-G.py
@@ -15,37 +15,42 @@ from math import sqrt, sin, pi
 #import timeit
 
 
-nx = 10 # no of interval in x-direction
-ny = 10 # # no of interval in x-direction
+nx = 50 # no of interval in x-direction
+ny = 50 # # no of interval in x-direction
 deltaX = 1/nx # length of an interval
-deltaY = deltaX
-nt = 1000 # number of time steps
-tmax = 10.0 #max. simulation time
-deltaT = tmax/nt # length of time interval
-d = 1.0 # diffusion coefficient
 deltaX2 = deltaX**2 
-deltaTMax = 4 * deltaT * d / deltaX2
-omega = 1 #needed for successive over-relaxation method
+deltaY = deltaX
+tmax = 10.0 #max. simulation time
+d = 1.0 # diffusion coefficient
+deltaT = (deltaX2 * 4 * d) / 10  # The max of delta t is deltaX2/(4*d), just to make sure we divide by 10
+steps = int(tmax / deltaT) + 1 # calculate the number of time steps
+omega = 1.5 #needed for successive over-relaxation method
+delta = 0.1
+tolerance = 10**(-5)
 
-
+"""
 #----------------START: JACOBI ITERATION-----------------------------------------
 u = np.zeros((nx+1, ny+1)) # 2D array for x-andy-values at current time
 ui = np.zeros((nx+1, ny+1)) # 2D array for x-andy-values at previous time
-timeListJ = []
+#itListJ = []
 
+for j in range(0, ny+1):
+	ui[0, j] = 1
 
 # calculate u from ui, calculate Laplacians
-for k in range(0, nt):
+while(tolerance < delta):
+	delta = 0
 	for j in range(0, ny+1):
-		ui[0, j] = 1
+		u[0, j] = 1
 		for i in range(1, nx):
 			u[i,j] = (ui[i-1,j] + ui[i,j-1] + ui[i+1,j] + ui[i,(j+1)%(ny+1)]) / 4
-	ui = u
-	timListJ.append(ui)
-	print(ui)
+	delta = np.max(np.abs(u - ui))
+	ui = np.copy(u)
+	print(delta)
 
 
-img = plt.imshow(ui, origin='upper') #RIGHT
+
+img = plt.imshow(u, origin='lower') #RIGHT
 plt.colorbar(img) #Right
 plt.show() #Right
 
@@ -53,50 +58,50 @@ plt.show() #Right
 
 #---------------------START: GAUSS-SEIDEL METHOD----------------------------------------------
 g = np.zeros((nx+1, ny+1)) # 2D array for x-and y-values at current time
-gi = np.zeros((nx+1, ny+1)) # 2D array for x-and y-values at previous time
-timeListG = []
 
 # calculate u from ui, calculate Laplacians
-for k in range(0, nt):
+while(delta > tolerance):
+	delta = 0
 	for j in range(0, ny+1):
-		gi[0, j] = 1
+		g[0, j] = 1
 		for i in range(1, nx):
-			g[i,j] = (g[i-1,j] + g[i,j-1] + gi[i+1,j] + gi[i,(j+1)%(ny+1)]) / 4
-	gi = g
-	timListG.append(gi)
-	print(ui)
+			value = np.copy(g[i,j])
+			g[i,j] = (g[i-1,j] + g[i,j-1] + g[i+1,j] + g[i,(j+1)%(ny+1)]) / 4
+			
+			compare = np.abs(g[i,j] - value)
+			if (compare > delta):
+				delta = compare
+	print(delta)
 
 
-img = plt.imshow(gi, origin='upper') #RIGHT
+img = plt.imshow(g, origin='lower') #RIGHT
 plt.colorbar(img) #Right
 plt.show() #Right
 
 #-------------------------------END: GAUSS-SEIDEL METHOD----------------------------------------------
 
+"""
 
 #------------------------------START: SUCCESSIVE OVER-RELAXATION------------------------------------
 s = np.zeros((nx+1, ny+1)) # 2D array for x-and y-values at current time
-si = np.zeros((nx+1, ny+1)) # 2D array for x-and y-values at previous time
-timeListS = []
+
 
 # calculate u from ui, calculate Laplacians
-for k in range(0, nt):
+while(delta > tolerance):
+	delta = 0
 	for j in range(0, ny+1):
-		si[0, j] = 1
+		s[0, j] = 1
 		for i in range(1, nx):
-			s[i,j] = (s[i-1,j] + s[i,j-1] + si[i+1,j] + si[i,(j+1)%(ny+1)]) / 4
-			s[i,j] = (1 - omega) * si[i,j] + omega * s[i,j]
-	si = s
-	timListG.append(gi)
-	print(ui)
+			value = np.copy(s[i,j])
+			s[i,j] = (omega / 4) * (s[i-1,j] + s[i,j-1] + s[i+1,j] + s[i,(j+1)%(ny+1)]) + (1 - omega) * s[i,j]
+			
+			compare = np.abs(s[i,j] - value)
+			if (compare > delta):
+				delta = compare
 
 
-img = plt.imshow(si, origin='upper') #RIGHT
-plt.colorbar(img) #Right
-plt.show() #Right
+print(countIterations)
 
-
-
-
-
-
+#img = plt.imshow(s, origin='lower') #RIGHT
+#plt.colorbar(img) #Right
+#plt.show() #Right
